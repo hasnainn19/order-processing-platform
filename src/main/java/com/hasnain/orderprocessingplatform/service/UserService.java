71b8093d6044117f8444aacaf683e137f1f6a3cb
Hasnain N <hasnainn3666@gmail.com>
2026-09-12 16:48:23 -0400

    Implement password hashing in UserService and add SecurityConfig for password encoding

--- src/main/java/com/hasnain/orderprocessingplatform/service/UserService.java
+++ src/main/java/com/hasnain/orderprocessingplatform/service/UserService.java
@@ -8,13 +8,17 @@ import com.hasnain.orderprocessingplatform.dto.CreateUserRequest;
 import com.hasnain.orderprocessingplatform.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Service 
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponse getUserById(Long id) {
@@ -27,7 +31,7 @@ public class UserService {
     public UserResponse createUser(CreateUserRequest request) {
         User user = new User();
         user.setEmail(request.email());
-        user.setPasswordHash(request.password()); // TODO: replace with hashed password
+        user.setPasswordHash(passwordEncoder.encode(request.password()));
         user.setRole(Role.USER);
 
         User saved = userRepository.save(user);
