750942eb50d2e572f3ff9af98f3e0ea43def18d0
Hasnain N <hasnainn3666@gmail.com>
2026-09-12 00:05:31 -0400

    Create ProductController

---
+++ src/main/java/com/hasnain/orderprocessingplatform/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.hasnain.orderprocessingplatform.controller;
+
+import com.hasnain.orderprocessingplatform.entity.Product;
+import com.hasnain.orderprocessingplatform.service.ProductService;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@RestController 
+@RequestMapping("/api/products")
+public class ProductController {
+    
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    } 
+}
