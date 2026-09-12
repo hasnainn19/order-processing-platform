16a8d4113a6e86923d9099b8641d8642220a0516
Hasnain N <hasnainn3666@gmail.com>
2026-09-11 22:31:45 -0400

    Initialize project and configure PostgreSQL datasource

---
+++ src/main/java/com/hasnain/orderprocessingplatform/OrderProcessingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hasnain.orderprocessingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasnain/orderprocessingplatform/OrderProcessingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasnain.orderprocessingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
