ab97fad23f33b7de3118331c898ec0ecf9fab00a
Hasnain N <hasnainn3666@gmail.com>
2026-09-11 23:43:16 -0400

    Create ProductService

---
+++ src/main/java/com/hasnain/orderprocessingplatform/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.hasnain.orderprocessingplatform.service;
+
+import com.hasnain.orderprocessingplatform.entity.Product;
+import com.hasnain.orderprocessingplatform.repository.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service 
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    
+    @Autowired 
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
